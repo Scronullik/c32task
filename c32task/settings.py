425c687702f3537c60747bbaf06fcdf795aadfd3
Andrey <scronullik@fastmail.com>
2021-12-14 14:04:38 +1100

    env

--- c32task/settings.py
+++ c32task/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from os import getenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6$racvo#5ahqzhpuaj&kbad&_dylpa)ypsf7%tgw7rj273(kyb'
+SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv('DEBUG', default=True)
 
 ALLOWED_HOSTS = []
 
